d55a415e651e033ae756b9595e452abeffcc5180
tchuby <danielandradesilva.84@gmail.com>
2023-04-03 14:40:29 -0300

    Calculator controller operations

---
+++ ApiRest/ApiRest.Api/Controllers/CalculatorController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ApiRest.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input.");
+        }
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSubtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+            return BadRequest("Invalid input.");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult GetDivision(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(division.ToString());
+            }
+            return BadRequest("Invalid input.");
+        }
+
+        [HttpGet("average/{firstNumber}/{secondNumber}")]
+        public IActionResult GetAverage(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var average = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(average.ToString());
+            }
+            return BadRequest("Invalid input.");
+        }
+
+        [HttpGet("squareroot/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSquareRoot(string numberInput)
+        {
+            try
+            {
+                var number = ConvertToDecimal(numberInput);
+                var subtraction = number * number;
+                return Ok(subtraction.ToString());
+            }
+            catch(Exception e){
+                _logger.LogError(e, "Invalid input.", null);
+                return BadRequest("Invalid input.");
+            }
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double numberOut;
+
+            bool isNumber = double.TryParse(
+                number, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out numberOut);
+
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+                return decimalValue;
+
+            return 0;
+        }
+    }
+}
